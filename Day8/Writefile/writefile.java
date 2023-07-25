f70b49ce7a3105c3c2dab1ceb448a581d0a50dc8
shubhankardutta <shubhankardutta38@gmail.com>
2023-07-25 15:31:37 +0530

    writefile_day8 updated

---
+++ Day8/Writefile/writefile.java
@@ -0,0 +1,17 @@
+package Writefile;
+import java.io.FileWriter; //filewriter class
+import java.io.IOException; //import the ISexception class
+public class writefile {
+    public static void main(String[] args) {
+        try{
+            FileWriter mywriter = new FileWriter("Users/shubhankardutta/Desktop/Learn Java/Day8/file.txt");
+            mywriter.write("File In java might be tricky,but it is fun enough");
+            mywriter.close();
+            System.out.println("Succesfully wrote to the file");
+        }
+        catch(IOException e){
+            System.out.println("An error Occured");
+            e.printStackTrace();
+        }
+    }
+}
