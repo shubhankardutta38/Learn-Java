c3f4c3aea12eb7e0646b2facf3d9d5963b706bc7
shubhankardutta <shubhankardutta38@gmail.com>
2023-08-01 20:04:50 +0530

    Day14_sql

---
+++ Day14/createtable.sql
@@ -0,0 +1,2 @@
+create table student_details
+(id int primary key auto_increment not null,name varchar(32) not null,address varchar(40) not null,phone_no int not null);
---
+++ Day14/desctable.sql
@@ -0,0 +1,1 @@
+desc student_details;
